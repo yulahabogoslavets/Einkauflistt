303afb73c2c677be2b0525ac0f137644f4cd87f2
yulahabogoslavets <speed0435@gmail.com>
2020-05-15 21:23:34 +0200

    Updates

---
+++ precache-manifest.72edf0adcbe8be487c4e75d7ce6caa33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da13cf1ce8f4a2a16f38550115f8de3",
+    "url": "/Einkauflistt/index.html"
+  },
+  {
+    "revision": "661ea17d1d2a37f2c031",
+    "url": "/Einkauflistt/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c52126ac6950e4c0c47c",
+    "url": "/Einkauflistt/static/js/2.15bb2c8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Einkauflistt/static/js/2.15bb2c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661ea17d1d2a37f2c031",
+    "url": "/Einkauflistt/static/js/main.64bc133d.chunk.js"
+  },
+  {
+    "revision": "e2f2b593963f0f63f897",
+    "url": "/Einkauflistt/static/js/runtime-main.9eee9565.js"
+  }
+]);
